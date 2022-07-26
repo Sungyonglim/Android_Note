7cbff958ec883249dbed36bf78a37f7bc83c722b
sungyong-lim <dlatjddyd524@gmail.com>
2022-07-26 17:45:50 +0900

    Dice Game Update

--- app/src/main/java/com/example/android_notes/activity/DiceActivity.kt
+++ app/src/main/java/com/example/android_notes/activity/DiceActivity.kt
@@ -3,62 +3,73 @@ package com.example.android_notes.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import com.example.android_notes.R
 import kotlinx.android.synthetic.main.activity_dice.*
 
 class DiceActivity : AppCompatActivity() {
 
+    private lateinit var diceFirst: ImageView
+    private lateinit var diceSecond: ImageView
+    private lateinit var startBtn: Button
+    private lateinit var clearBtn: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dice)
 
-        iv_dice.setImageResource(R.drawable.ic_dice_bg)
+        // 주사위 아이디
+        diceFirst = findViewById(R.id.dice_image)
+        diceSecond = findViewById(R.id.dice_image2)
+        // 버튼 아이디
+        startBtn = findViewById(R.id.start_btn)
+        clearBtn = findViewById(R.id.clear_btn)
 
-        onClick(result_btn)
-        onClick(clear_btn)
+        onClick(startBtn)
+        onClick(clearBtn)
+
+
 
     }
 
-    private fun randomValue(): Int {
-        return (1..6).random()
-    }
-
+    // 버튼 클릭 메서드
     private fun onClick(v: View) {
         when(v.id) {
-            R.id.result_btn -> result_btn.setOnClickListener {
+            R.id.start_btn -> startBtn.setOnClickListener { rollDice() }
 
-                when(randomValue()) {
-                    1 -> {
-                        iv_dice.setImageResource(R.drawable.ic_dice_one)
-                        tv_text.text = "1"
-                    }
-                    2 -> {
-                        iv_dice.setImageResource(R.drawable.ic_dice_two)
-                        tv_text.text = "2"
-                    }
-                    3 -> {
-                        iv_dice.setImageResource(R.drawable.ic_dice_three)
-                        tv_text.text = "3"
-                    }
-                    4 -> {
-                        iv_dice.setImageResource(R.drawable.ic_dice_four)
-                        tv_text.text = "4"
-                    }
-                    5 -> {
-                        iv_dice.setImageResource(R.drawable.ic_dice_five)
-                        tv_text.text = "5"
-                    }
-                    6 -> {
-                        iv_dice.setImageResource(R.drawable.ic_dice_six)
-                        tv_text.text = "6"
-                    }
-                }
+            R.id.clear_btn -> clearBtn.setOnClickListener {
+                diceFirst.setImageResource(R.drawable.empty_dice)
+                diceSecond.setImageResource(R.drawable.empty_dice)
             }
-            R.id.clear_btn -> clear_btn.setOnClickListener {
-                tv_text.text = "0"
-                iv_dice.setImageResource(R.drawable.ic_dice_bg)
-            }
+
         }
     }
 
+
+    // 주사위 이미지 변경 메서드
+    private fun rollDice() {
+        diceFirst.setImageResource(getRandomDiceImage()) // getRandomDiceImage 에서 받아온 이미지 설정
+        diceSecond.setImageResource(getRandomDiceImage())
+    }
+
+    // 랜덤 주사위 추출 메서드
+    private fun getRandomDiceImage(): Int {
+        val randomInt = (1..6).random() // 1 ~ 6 사이 숫자 할당
+
+        return when(randomInt) { // 랜덤한 숫자에 따라 이미지 부여
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+    }
+
+
 }
+
