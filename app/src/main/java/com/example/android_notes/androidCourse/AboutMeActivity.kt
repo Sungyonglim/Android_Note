5e2692517cc725df7ff098917a2dcb0a23d3cdfc
sungyong-lim <dlatjddyd524@gmail.com>
2022-07-28 16:31:03 +0900

    Update DataBinding, Comment

--- app/src/main/java/com/example/android_notes/androidCourse/AboutMeActivity.kt
+++ app/src/main/java/com/example/android_notes/androidCourse/AboutMeActivity.kt
@@ -6,22 +6,37 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.DataBindingUtil
 import com.example.android_notes.R
 import com.example.android_notes.databinding.ActivityAboutMeBinding
+
+/** 뷰를 데이터에 바인딩하는 단계**/
+/*
+    1. 데이터에 대한 데이터 클래스를 만듬
+    2. <layout> 태그 안에 <data> 블록을 추가
+    3. <variable> 태그 안에 데이터 클래스의 유형을 정의함 
+    4. Activity 에서 데이터 클래스의 인스턴스로 변수를 만듬 
+    5. 바인딩 개체에서 방금 만든 변수로 변수를 설정
+    6. XML 에서 보기의 내용을 <data> 블록에 정의한 변수로 설정.
+       점 표기법을 사용하여 데이터 클래스 내부의 데이터에 액세스함 (android: text = "@={myName.name}")
+* */
 
 class AboutMeActivity : AppCompatActivity() {
 
     // 바인딩 개체에 대한 변수 생성
     private lateinit var binding: ActivityAboutMeBinding
+    // 4번 
+    private val myName: MyName = MyName("Sungyong Lim")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_about_me)
         // DataBindingUtil 클래스의 setContentView() 함수를 사용하여 "activity_about_me"와 연결
         binding = DataBindingUtil.setContentView(this, R.layout.activity_about_me)
-
+        
+        // 5번
+        binding.myName = myName
+        
         onClick(binding.doneButton)
         onClick(binding.nicknameText)
 
@@ -44,8 +59,8 @@ class AboutMeActivity : AppCompatActivity() {
                 Toast.makeText(this ,"닉네임을 입력해주세요.ㅁㄴ", Toast.LENGTH_SHORT).show()
             } else {
                 binding.apply {
-                    nicknameText.text = nicknameEdit.text.toString() // 입력한 editText text 를 textView text 에 할당
-
+                    myName?.nickName = nicknameEdit.text.toString() // 입력한 editText text 를 textView text 에 할당
+                    invalidateAll() // 닉네임이 설정된 후 코드에서 새 데이터로 ui를 새로 고쳐야 하기 때문에 올바른 데이터로 다시 생성되도록 모든 바이딩 식을 무효화
                     nicknameEdit.visibility = View.GONE // editText 숨김
                     v.visibility = View.GONE // button 숨김
                     nicknameText.visibility = View.VISIBLE // TextView 보이게 설정
---
+++ app/src/main/java/com/example/android_notes/androidCourse/MyName.kt
@@ -0,0 +1,3 @@
+package com.example.android_notes.androidCourse
+
+data class MyName(var name: String = "", var nickName: String = "")
