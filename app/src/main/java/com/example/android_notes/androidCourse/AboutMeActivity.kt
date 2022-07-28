91733141fcd714e4310a0650914da36bdfc38231
sungyong-lim <dlatjddyd524@gmail.com>
2022-07-28 12:56:45 +0900

    Create AboutMe Layout

---
+++ app/src/main/java/com/example/android_notes/androidCourse/AboutMeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.android_notes.androidCourse
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android_notes.R
+
+class AboutMeActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_me)
+    }
+}
---
+++ app/src/main/java/com/example/android_notes/androidCourse/DiceActivity.kt
@@ -0,0 +1,73 @@
+package com.example.android_notes.androidCourse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import com.example.android_notes.R
+
+class DiceActivity : AppCompatActivity() {
+
+    private lateinit var diceFirst: ImageView
+    private lateinit var diceSecond: ImageView
+    private lateinit var startBtn: Button
+    private lateinit var clearBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dice)
+
+        // 주사위 아이디
+        diceFirst = findViewById(R.id.dice_image)
+        diceSecond = findViewById(R.id.dice_image2)
+
+        // 버튼 아이디
+        startBtn = findViewById(R.id.start_btn)
+        clearBtn = findViewById(R.id.clear_btn)
+
+
+        onClick(startBtn)
+        onClick(clearBtn)
+
+
+    }
+
+    private fun onClick(v: View) {
+        when(v.id) {
+            R.id.start_btn -> startBtn.setOnClickListener {
+                rollDice()
+            }
+            R.id.clear_btn -> clearBtn.setOnClickListener {
+                diceFirst.setImageResource(R.drawable.empty_dice)
+                diceSecond.setImageResource(R.drawable.empty_dice)
+            }
+
+        }
+    }
+
+
+
+    // 주사위 이미지 변경 메서드
+    private fun rollDice() {
+        diceFirst.setImageResource(getRandomDiceImage()) // getRandomDiceImage 에서 받아온 이미지 설정
+        diceSecond.setImageResource(getRandomDiceImage())
+    }
+
+    // 랜덤 주사위 추출 메서드
+    private fun getRandomDiceImage(): Int {
+
+        return when((1..6).random()) { // 1 ~ 6 사이의 랜덤한 숫자에 따라 이미지 부여
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+    }
+
+
+}
+
