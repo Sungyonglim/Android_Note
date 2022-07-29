be12070250595d52c0402dc8cd1048b2ffa65105
sungyong-lim <dlatjddyd524@gmail.com>
2022-07-29 16:53:04 +0900

    Create SignIn

--- app/src/main/java/com/example/android_notes/sampleApp/SampleMainActivity.kt
+++ app/src/main/java/com/example/android_notes/sampleApp/SampleMainActivity.kt
@@ -17,17 +17,19 @@ class SampleMainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivitySampleMainBinding
-
+    private lateinit var email: EditText
+    private lateinit var pwd: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_sample_main)
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sample_main)
 
         auth = Firebase.auth
 
-        val signBtnClicked = findViewById<Button>(R.id.signBtn)
-        signBtnClicked.setOnClickListener {
+        email = binding.emailArea
+        pwd = binding.pwdArea
+
+        // 회원가입
+        binding.signBtn.setOnClickListener {
 
             // email, password 매개변수로 받아오는 방법 1
             // EditText id 를 찾고 매개변수에 형변화해서 할당
@@ -36,10 +38,8 @@ class SampleMainActivity : AppCompatActivity() {
 
             // 방법 2
             // DataBinding
-            val email = binding.emailArea
-            val pwd = binding.pwdArea
 
-            // 회원가입 
+
             auth.createUserWithEmailAndPassword(email.text.toString(), pwd.text.toString())
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
@@ -49,14 +49,25 @@ class SampleMainActivity : AppCompatActivity() {
 
                     }
                 }
+        }
 
-            
-            // 로그아웃
-            binding.logoutBtn.setOnClickListener {
-                auth.signOut()
-                Toast.makeText(this, auth.currentUser?.uid.toString(), Toast.LENGTH_SHORT).show()
-            }
+        // 로그인
+        binding.loginBtn.setOnClickListener {
+            auth.signInWithEmailAndPassword(email.text.toString(), pwd.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        Toast.makeText(this, "${user?.email.toString()}님 로그인 성공", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
 
+        // 로그아웃
+        binding.logoutBtn.setOnClickListener {
+            auth.signOut()
+            Toast.makeText(this, auth.currentUser?.uid.toString(), Toast.LENGTH_SHORT).show()
         }
     }
 }
