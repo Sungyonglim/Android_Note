8f56ebecc0cf43ec03eb393621e5617acc64d4b5
dlatjddyd524@gmail.com <dlatjddyd524@gmail.com>
2022-09-18 17:11:32 +0900

    Update

---
+++ app/src/main/java/com/example/android_notes/jetpack/first/FirstActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android_notes.jetpack.first
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android_notes.R
+
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+    }
+}
---
+++ app/src/main/java/com/example/android_notes/jetpack/second/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android_notes.jetpack.second
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android_notes.R
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
