97f6afc663bdfc022ad80ea4a166205bfe17fd71
dlatjddyd524@gmail.com <dlatjddyd524@gmail.com>
2022-09-19 18:31:37 +0900

    Update splash screen

---
+++ app/src/main/java/com/example/android_notes/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.example.android_notes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+    }
+}
