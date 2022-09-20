0b7b48fbc392d57cb68deb535a4d2a15856ae6d7
dlatjddyd524@gmail.com <dlatjddyd524@gmail.com>
2022-09-20 20:44:59 +0900

    위치 권한 거부 시 보여주는 다이얼로그 구현

--- app/src/main/java/com/example/android_notes/permissions/PermissionsActivity.kt
+++ app/src/main/java/com/example/android_notes/permissions/PermissionsActivity.kt
@@ -1,27 +1,30 @@
 package com.example.android_notes.permissions
 
 import android.Manifest
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.view.View
 import android.view.ViewTreeObserver
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import com.example.android_notes.R
 import kotlin.concurrent.thread
 
 class PermissionsActivity : AppCompatActivity() {
-    private val TAG = PermissionsActivity::class.java.simpleName
     private var isReady = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_permissions)
 
         thread(true) {
             for (i in 1..3) {
-                Log.d(TAG, "sleep")
                 Thread.sleep(1000)
             }
             isReady = true
@@ -39,6 +42,9 @@ class PermissionsActivity : AppCompatActivity() {
                     permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
                         Toast.makeText(this, "Coarse OK", Toast.LENGTH_SHORT).show()
                     }
+                    else -> {
+                        showDialogForPermissions()
+                    }
                 }
             }
         }
@@ -55,7 +61,6 @@ class PermissionsActivity : AppCompatActivity() {
                                 Manifest.permission.ACCESS_COARSE_LOCATION
                             )
                         )
-
                         true
                     } else {
                         false
@@ -63,5 +68,21 @@ class PermissionsActivity : AppCompatActivity() {
                 }
             }
         )
+    }
+
+    private fun showDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("권한 거부 -> 앱 설정에 활성화")
+            .setPositiveButton("설정") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("취소") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 }
